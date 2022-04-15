a65cb04411e37295e954e5d6921f6dc3625a5377
Max <maxslon101@gmail.com>
2022-04-15 11:47:17 +0300

    First changes

--- app/src/main/java/com/max/natifeasync/LiveDataFragment.kt
+++ app/src/main/java/com/max/natifeasync/LiveDataFragment.kt
@@ -1,7 +1,6 @@
 package com.max.natifeasync
 
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
